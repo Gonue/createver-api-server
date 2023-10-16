c661fa2a2a50b392ef11d51f8ec31a353aa94c43
Gonue <6017891@naver.com>
2023-10-16 11:12:57 +0900

    fix : header design

--- client/src/components/layout/Header.vue
+++ client/src/components/layout/Header.vue
@@ -1,44 +1,83 @@
 <template>
     <div>
-        <body>
-            <main>
-                <header class="p-3 text-bg">
-                    <div class="container">
-                        <div class="row">
-                            <div
-                                class="col d-flex flex-wrap align-items-center justify-content-center justify-content-start">
-                                <a href="/" id="logo" class="col-3 d-flex align-items-center justify-content-center mb-lg-0 link-body-emphasis text-decoration-none text-white fw-bold">
-                                    <img src="https://github.com/Gonue/mine/assets/109960034/d75f1bc8-cf6b-4aee-8b7a-262d2b22e21f" width="32" height="32">Createver
-                                </a>
-                                <ul id="con" class="nav col-12 col-lg-auto ms-lg-auto justify-content-lg-end mb-2 mb-md-0">
-                                    <li><a href="/" class="nav-link text-white">Home</a></li>
-                                    <li><a href="/image" class="nav-link text-white">Image</a></li>
-                                    <li><a href="/community" class="nav-link text-white">Community</a></li>
-                                    <li><a href="/community" class="nav-link text-white">Community</a></li>
-                                    <li v-if="!token">
-                                        <a class="custom-btn nav-link text-white fw-bold" href="/login">Log-In</a>
-                                    </li>
-                                </ul>
-                                    <div v-if="token" class="fw-bold mx-3 text-white">{{ nickName }}</div>
-                                    <div v-if="token" class="dropdown text-end">
-                                        <a href="#" class="d-block link-body-emphasis text-decoration-none" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <img :src="profileImage" alt="profile" width="32" height="32"
-                                                class="rounded-circle">
-                                        </a>
-                                        <ul class="dropdown-menu dropdown-menu-dark text-small">
-                                            <li><a class="dropdown-item" href="/mypage">My Page</a></li>
-                                            <li>
-                                                <hr class="dropdown-divider">
-                                            </li>
-                                            <li><a class="dropdown-item" @click="logout" href="#">Sign out</a></li>
-                                        </ul>
-                                </div>
+        <header class="p-3 text-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col d-flex flex-wrap align-items-center justify-content-between">
+                        <!-- 햄버거 메뉴 (모바일에서만 보임) -->
+                        <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                                class="bi bi-list text-white" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                            </svg>
+                        </button>
+
+                        <!-- Offcanvas 컴포넌트 -->
+                        <div class="offcanvas offcanvas-start bg-dark" tabindex="-2" id="offcanvasExample"
+                            aria-labelledby="offcanvasExampleLabel">
+                            <div class="offcanvas-header">
+                                <h5 id="offcanvasExampleLabel text-white"></h5>
+                                <button type="button" class="btn-close text-reset bg-white" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <!-- 컨텐츠 -->
+                                <a href="/" class="nav-link text-white"><h2>Home</h2></a>
+                                <a href="/image" class="nav-link text-white"><h2>Image</h2></a>
+                                <a href="/community" class="nav-link text-white"><h2>Community</h2></a>
                             </div>
                         </div>
+
+                        <!-- 로고 -->
+                        <a href="/" id="logo"
+                            class=" d-flex align-items-center justify-content-center mb-lg-0 link-body-emphasis text-decoration-none text-white fw-bold">
+                            <img src="https://github.com/Gonue/mine/assets/109960034/d75f1bc8-cf6b-4aee-8b7a-262d2b22e21f"
+                                width="32" height="32">Createver
+                        </a>
+
+
+                        <!-- 데스크톱 메뉴 -->
+                        <ul id="con" class="nav d-none d-lg-flex ms-lg-auto mb-2 mb-md-0">
+                            <li><a href="/" class="nav-link text-white">Home</a></li>
+                            <li><a href="/image" class="nav-link text-white">Image</a></li>
+                            <li><a href="/community" class="nav-link text-white">Community</a></li>
+                        </ul>
+
+                        <div class="d-flex align-items-center ml-lg-auto">
+                            <div v-if="!token" class="d-lg-block">
+                                <a class="custom-btn nav-link text-white fw-bold" href="/login">Log-In</a>
+                            </div>
+                            <div v-if="token" class="fw-bold mx-3 text-white">{{ nickName }}</div>
+                            <div v-if="token" class="dropdown text-end">
+                                <a href="#" class="d-block link-body-emphasis text-decoration-none"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img :src="profileImage" alt="profile" width="32" height="32" class="rounded-circle">
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark text-small">
+                                    <li><a class="dropdown-item" href="/mypage">My Page</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" @click="logout" href="#">Sign out</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <!-- 모바일 메뉴 -->
+                        <div class="collapse navbar-collapse d-lg-none" id="navbarNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item"><a class="nav-link" href="/image">Image</a></li>
+                                <li class="nav-item"><a class="nav-link" href="/community">Community</a></li>
+
+                            </ul>
+                        </div>
                     </div>
-                </header>
-            </main>
-        </body>
+                </div>
+            </div>
+        </header>
     </div>
 </template>
 
@@ -47,7 +86,7 @@ export default {
     name: "headerView",
 
     methods: {
-        logout(){
+        logout() {
             this.$store.dispatch('logout');
         }
     },
@@ -68,11 +107,14 @@ export default {
 
 <style scoped>
 body {
-    font-family:'Courier New', Courier, monospace;
+    font-family: 'Courier New', Courier, monospace;
 }
-.text-bg{
+
+.text-bg {
     background-color: #141827;
-    border-bottom: 0.1px solid #424242;  /* 2px 높이의 흰색 밑줄 */
+    border-bottom: 0.1px solid #424242;
+    color: white;
+    /* 2px 높이의 흰색 밑줄 */
 
 }
 
@@ -90,6 +132,20 @@ body {
 }
 
 #logo img {
-    margin-right: 10px;
+    margin-left: 20px;
+}
+
+.offcanvas {
+    width: 250px !important;
+}
+
+.offcanvas-body a {
+    border-bottom: 1px solid #515b65;
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+}
+
+.offcanvas-body a:last-child {
+    border-bottom: none;
 }
 </style>
