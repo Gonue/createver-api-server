03d9b8113cb357ce837a50ee562644d39f8a146d
Gonue <6017891@naver.com>
2023-10-25 03:34:41 +0900

    feat : add image view
    - 조회 추가
    - 통계
    - 사용자별 조회 이력

---
+++ src/main/java/com/template/server/domain/image/entity/ImageView.java
@@ -0,0 +1,46 @@
+package com.template.server.domain.image.entity;
+
+import com.template.server.domain.member.entity.Member;
+import com.template.server.global.audit.AuditingFields;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@Table(name = "image_view",
+       indexes = {@Index(name = "idx_member", columnList = "member_id"),
+                  @Index(name = "idx_gallery", columnList = "gallery_id"),
+                  @Index(name = "idx_viewed_at", columnList = "viewed_at")})
+@Entity
+public class ImageView extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "view_id", updatable = false)
+    private Long viewId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gallery_id")
+    private Gallery gallery;
+
+    @Column(name = "viewed_at")
+    private LocalDateTime viewedAt;
+
+    public static ImageView create(Member member, Gallery gallery) {
+        ImageView imageView = new ImageView();
+        imageView.setMember(member);
+        imageView.setGallery(gallery);
+        imageView.setViewedAt(LocalDateTime.now());
+        return imageView;
+    }
+}
