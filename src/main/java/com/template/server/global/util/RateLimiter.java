989003ddb0fb39b881e50d308edef0d6ce1663cf
Gonue <6017891@naver.com>
2023-10-28 06:46:40 +0900

    feat : add RateLimiter, RateLimiterManager Class
    - RateLimiter : 토큰 버킷 알고리즘을 사용하여 요청 제한, 각 사용자는 토큰이라는 일종의 자원 유지 및 사용자가 요청을 보낼 때마다 토큰 사용, 토큰은 일정 시간 간격 재충전, 토큰이 없으면 요청 거부(429)
    
    - RateLimiterManager : 사용자의 이메일 기반으로 RateLimiter 인스턴스 관리, 사용자가 요청을 보낼때 RateLimiterManager는 해당 사용자의 RateLimiter 인스턴스를 찾아 요청 허용할지 여부 결정, 지금의 경우 요건이 애매한 부분이 있음.
    
    - 비로그인 사용자에 대해 개별의 rate limit 적용이 애매함. ip기반으로 하자니 프록시 및 로드밸런서 때문에 적용이 애매하고 브라우저 지문이나, 쿠키 기반의 레이트 리밋 방식이 필요해보인다.

---
+++ src/main/java/com/template/server/global/util/RateLimiter.java
@@ -0,0 +1,29 @@
+package com.template.server.global.util;
+
+public class RateLimiter {
+    private final int maxTokens;
+    private final long refillInterval;
+    private int tokens;
+    private long nextRefillTime;
+
+    public RateLimiter(int maxTokens, long refillInterval) {
+        this.maxTokens = maxTokens;
+        this.refillInterval = refillInterval;
+        this.tokens = maxTokens;
+        this.nextRefillTime = System.nanoTime() + refillInterval;
+    }
+
+    public synchronized boolean allowRequest() {
+        long now = System.nanoTime();
+        if (now > nextRefillTime) {
+            tokens = maxTokens;
+            nextRefillTime = now + refillInterval;
+        }
+        if (tokens > 0) {
+            tokens--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
