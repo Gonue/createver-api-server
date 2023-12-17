8a3da5b72ec8b489f50db4aa28e43078883dc55d
Gonue <6017891@naver.com>
2023-12-18 00:53:11 +0900

    feat : Refresh Token Redis 저장 로직 추가
    - 리프레시 토큰을 Redis에 저장하는 로직을 추가
    - 서버 측에서 리프레시 토큰의 유효성을 보다 효과적으로 제어가능.
    - 탈취된 토큰의 사용을 방지 및 필요 시 즉각적으로 토큰을 무효화 가능

--- src/main/java/com/createver/server/global/auth/service/AuthService.java
+++ src/main/java/com/createver/server/global/auth/service/AuthService.java
@@ -5,6 +5,7 @@ import com.createver.server.global.auth.utils.CustomAuthorityUtils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,15 +19,23 @@ public class AuthService {
 
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils customAuthorityUtils;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public String refreshAccessToken(String refreshToken) {
         try {
+            // Redis에서 리프레시 토큰 검증
             String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
             Jws<Claims> claimsJws = jwtTokenizer.getClaims(refreshToken, base64EncodedSecretKey);
             String email = claimsJws.getBody().getSubject();
 
-            List<String> roles = customAuthorityUtils.createRoles(email);
+            // Redis에서 리프레시 토큰 확인
+            String storedRefreshToken = redisTemplate.opsForValue().get("REFRESH_TOKEN:" + email);
+            if (storedRefreshToken == null || !storedRefreshToken.equals(refreshToken)) {
+                throw new IllegalArgumentException("Invalid or expired refresh token");
+            }
 
+            // 새 액세스 토큰 생성
+            List<String> roles = customAuthorityUtils.createRoles(email);
             Map<String, Object> claims = new HashMap<>();
             claims.put("email", email);
             claims.put("roles", roles);
@@ -38,4 +47,10 @@ public class AuthService {
             throw new IllegalArgumentException("Invalid refresh token", e);
         }
     }
+
+    public void deleteRefreshToken(String email) {
+        // Redis에서 리프레쉬 토큰 삭제
+        redisTemplate.delete("REFRESH_TOKEN:" + email);
+    }
+
 }
